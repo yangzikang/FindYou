49c27f023a51fb1dfce6e93b89f09c5d4164717e
unknown <986762402@qq.com>
2017-03-12 03:19:37 +0800

    new one

--- app/src/main/java/com/example/dell/findyou/MainActivity.java
+++ app/src/main/java/com/example/dell/findyou/MainActivity.java
@@ -7,22 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+import static android.R.attr.onClick;
 
 public class MainActivity extends Activity {
+    @BindView(R.id.enter) Button enter;
+    @OnClick(R.id.enter)
+    void enter() {
+        Intent intent = new Intent(MainActivity.this,LocationActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
-
-        Button enter = (Button)findViewById(R.id.enter);
-        enter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,LocationActivity.class);
-                startActivity(intent);
-            }
-        });
+        ButterKnife.bind(this);
     }
 }
